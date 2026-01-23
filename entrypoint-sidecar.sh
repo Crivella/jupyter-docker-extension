84c7c7cad22bc02e1b56c7d46d47ee8aedf966be
crivella <davidecrivella@yahoo.it>
2026-01-23 17:48:47 +0100

    WIP test using kernels and other extensions

--- entrypoint-sidecar.sh
+++ entrypoint-sidecar.sh
@@ -20,20 +20,43 @@ mount -t cvmfs software.eessi.io /cvmfs/software.eessi.io
 cd /home/${USER}
 # rm -fr .jupyter
 su -c '
-# source /cvmfs/software.eessi.io/versions/2023.06/init/bash 
+source /cvmfs/software.eessi.io/versions/2023.06/init/bash 
 # module load EESSI-extend
 # eb jupyterlmod-4.0.3-GCCcore-12.3.0.eb -r 
-# # eb jupyterlmod-4.0.3-GCCcore-12.3.0.eb -r --allow-use-as-root-and-accept-consequences
 # module load jupyterlmod/4.0.3-GCCcore-12.3.0
 # export JUPYTER_PATH="$EBROOTJUPYTERLMOD/share/jupyter:$JUPYTER_PATH"
-# module load JupyterLab
+module load JupyterLab
 
-source /cvmfs/software.eessi.io/versions/2025.06/init/bash 
-module load EESSI-extend
-eb jupyterlmod-5.2.2-GCCcore-13.3.0.eb -r 
-module load jupyterlmod/5.2.2-GCCcore-13.3.0
-export JUPYTER_PATH="$EBROOTJUPYTERLMOD/share/jupyter:$JUPYTER_PATH"
-module load JupyterLab/4.2.5-GCCcore-13.3.0
+pip install --user jupyterlmod
+pip install --user octave_kernel
+module load Octave/10.1.0
+python -m octave_kernel install --user
+
+# # ----------------------------------------------------------------
+# module load Python
+# export PATH="$HOME/.local/bin:$PATH"
+# pip install --user --upgrade pip
+# pip install --user jupyter jupyter-server-proxy
+# pip install --user jupyter-code-server
+# pip install --user pip install jupyter-rsession-proxy
+# pip install --user jupyterlmod
+
+# module load EESSI-extend
+# eb code-server-4.105.1.eb - r
+# export JSP_CODE_SERVER_LMOD_MODULE=code-server/4.105.1
+# export CODE_DISABLE_PASSWORD=true
+
+# pip install --user octave_kernel
+# module load Octave/10.1.0
+# python -m octave_kernel install --user
+# # ----------------------------------------------------------------
+
+# source /cvmfs/software.eessi.io/versions/2025.06/init/bash 
+# module load EESSI-extend
+# eb jupyterlmod-5.2.2-GCCcore-13.3.0.eb -r 
+# module load jupyterlmod/5.2.2-GCCcore-13.3.0
+# export JUPYTER_PATH="$EBROOTJUPYTERLMOD/share/jupyter:$JUPYTER_PATH"
+# module load JupyterLab/4.2.5-GCCcore-13.3.0
 
 export -f module
 
@@ -44,3 +67,5 @@ jupyter lab \
     --allow-root \
     --ip 0.0.0.0
 ' ${USER}
+
+tail -f /dev/null
