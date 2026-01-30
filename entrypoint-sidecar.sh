863f8ec58d185687533bcb4129969c16e289e531
crivella <davidecrivella@yahoo.it>
2026-01-30 22:29:47 +0100

    jupyterlmod on EESSI

--- entrypoint-sidecar.sh
+++ entrypoint-sidecar.sh
@@ -38,64 +38,6 @@ export OMPI_MCA_pml=^ucx
 export OMPI_MCA_mtl=^ofi                                      
 export OMPI_MCA_btl_tcp_if_exclude=docker0,127.0.0.0/8 
 
-module load EESSI-extend
-
-eb jupyterlmod-4.0.3-GCCcore-12.3.0.eb -r 
-
-module load JupyterLab
-module load jupyterlmod/4.0.3-GCCcore-12.3.0
-CHECK="`jupyter labextension list 2>&1 | grep lmod`"
-module purge
-module load EESSI-extend
-
-if [ -z "$CHECK" ]; then
-    # echo "REBUILDING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-    eb jupyter-server-2.7.2-GCCcore-12.3.0.eb --rebuild
-else
-    # echo "NOT REBUILDING-------------------------------------------------------"
-fi
-
-eb --from-pr 25155 -r
-
-# module load jupyterlmod/4.0.3-GCCcore-12.3.0
-# export JUPYTER_PATH="$EBROOTJUPYTERLMOD/share/jupyter:$JUPYTER_PATH"
-# module load Jupyter-bundle/20230823-GCCcore-12.3.0
-
-# pip install --user jupyterlmod
-# # echo "Installing octave-kernel"
-# pip install --user octave_kernel
-# module load Octave/10.1.0
-# export JUPYTER_PATH="/home/eessi-user/.local/lib/python3.11/site-packages/octave_kernel:$JUPYTER_PATH"
-# python -m octave_kernel install --user
-
-# # ----------------------------------------------------------------
-# module load Python
-# export PATH="$HOME/.local/bin:$PATH"
-# pip install --user --upgrade pip
-# pip install --user jupyter jupyter-server-proxy
-# pip install --user jupyter-code-server
-# pip install --user pip install jupyter-rsession-proxy
-# pip install --user jupyterlmod
-
-# module load EESSI-extend
-# eb code-server-4.105.1.eb - r
-# export JSP_CODE_SERVER_LMOD_MODULE=code-server/4.105.1
-# export CODE_DISABLE_PASSWORD=true
-
-# pip install --user octave_kernel
-# module load Octave/10.1.0
-# python -m octave_kernel install --user
-# # ----------------------------------------------------------------
-
-# source /cvmfs/software.eessi.io/versions/2025.06/init/bash 
-# module load EESSI-extend
-# eb jupyterlmod-5.2.2-GCCcore-13.3.0.eb -r 
-# module load jupyterlmod/5.2.2-GCCcore-13.3.0
-# export JUPYTER_PATH="$EBROOTJUPYTERLMOD/share/jupyter:$JUPYTER_PATH"
-# module load JupyterLab/4.2.5-GCCcore-13.3.0
-
-# export -f module
-
 module load JupyterLab
 module load jupyterlmod/4.0.3-GCCcore-12.3.0
 
